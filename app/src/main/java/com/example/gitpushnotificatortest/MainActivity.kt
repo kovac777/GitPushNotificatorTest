e9a2411f28062ed9a101bc43b152beb292205bea
maksim <kovac619@gmail.com>
2019-06-09 15:24:14 +0300

    remove logs

--- app/src/main/java/com/example/gitpushnotificatortest/MainActivity.kt
+++ app/src/main/java/com/example/gitpushnotificatortest/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.gitpushnotificatortest
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("logi", "onCreate : ")
+
     }
 }
